0b9b7aaca871178755e43736237941e69ff711a3
Syahri Ramadhani <syahri.ramadhani@sirclo.com>
2022-12-26 13:45:54 +0800

    fixing wrong push code

---
+++ application/controllers/CDashboard.php
@@ -0,0 +1,131 @@
+<?php
+class CDashboard extends CI_Controller{
+    function __construct(){
+        parent:: __construct();
+		$this->load->library('CSVReader');
+		$this->load->helper(array('form', 'url'));
+    }
+    function index(){
+        $this->load->view('index');
+    }
+	// fungsi cari max attribute
+	function max_attribute_in_array($array, $prop) {
+		return max(array_map(function($o) use($prop) {
+								return $o[$prop];
+							 },
+							 $array));
+	} 
+	function do_upload(){
+		// Kriteria
+		$kriteria = [
+			["namaKriteria" => "Diklat", "bobot" => "0.2"],
+			["namaKriteria" => "Sertifikasi", "bobot" => "0.3"],
+			["namaKriteria" => "Bahasa", "bobot" => "0.1"],
+			["namaKriteria" => "TPA", "bobot" => "0.2"],
+			["namaKriteria" => "Nilai_SKP", "bobot" => "0.2"],
+		
+		];
+		$csvData = $this->csvreader->parse_csv($_FILES['userfile']['tmp_name']);
+		if(!empty($csvData)){
+			$arrayDataPegawai = array();
+			foreach($csvData as $row){ 
+				$memData = array(
+					'Pegawai' => $row['pegawai'],
+					'pegawai_pendidikan' => $row['pegawai_pendidikan'],
+					'pegawai_perwakilan' => $row['perwakilan'],
+					'jabatan_nama' => $row['jabatan_nama'],
+					'Diklat' => $row['diklat'],
+					'Sertifikasi' => $row['sertifikasi'],
+					'Bahasa' => $row['bahasa'],
+					'TPA' => $row['tpa'],
+					'Nilai_SKP' => $row['nilai_skp'],
+					'Tahun' => $row['tahun'],
+				);
+				array_push($arrayDataPegawai,$memData);
+			}
+			
+			  //hasil max setiap kriteria dalam bentuk array
+			  $hasilMaxDariSetiapKriteria = array();
+			  $nilaiFinal = array();
+                
+			  //hasil pembagian antara nilai kriteria dan nilai max kriteria
+			  $nilaiHasilBagiAntaraNilaiKriteriaDanNilaiMaxKriteria = array();
+
+				//   Cari nilai max disetiap kriteria
+			  foreach ($kriteria as $k) {
+				$max = $this -> max_attribute_in_array($arrayDataPegawai, $k['namaKriteria']);
+				array_push($hasilMaxDariSetiapKriteria, array(
+                    'nama_kriteria' => $k['namaKriteria'],
+                    'hasil' => $max,
+                ));
+			  }
+		
+			  	// Hitung Normalisasi Benefit dan assign ke setiap pegawai
+				foreach ($arrayDataPegawai as $n) {
+					$c = array();
+					foreach ($hasilMaxDariSetiapKriteria as $hmdsk) {
+						array_push($c, array(
+							'nama_kriteria' => $hmdsk['nama_kriteria'],
+							'hasil' => $n[$hmdsk['nama_kriteria']] / $hmdsk['hasil'],
+						));
+					}
+					array_push($nilaiHasilBagiAntaraNilaiKriteriaDanNilaiMaxKriteria, array(
+						'Pegawai' => $n['Pegawai'],
+						'pegawai_pendidikan' => $n['pegawai_pendidikan'],
+						'pegawai_perwakilan' => $n['pegawai_perwakilan'],
+						'jabatan_nama' => $n['jabatan_nama'],
+						'tahun' => $n['Tahun'],
+						'hasil_akhir' => $c,
+					));
+				}
+
+				// Dibobotin dan nambah nilai akhir
+				foreach ($nilaiHasilBagiAntaraNilaiKriteriaDanNilaiMaxKriteria as $n) {
+				$val = array(
+					'Pegawai' => $n['Pegawai'],
+					'Pendidikan' => $n['pegawai_pendidikan'],
+					'Jabatan' => $n['jabatan_nama'],
+					'Tahun' => $n['tahun'],
+					'Pegawai Perwakilan' => $n['pegawai_perwakilan'],
+				
+					'Diklat' => ($n['hasil_akhir'][0]['hasil'] * $kriteria[0]['bobot']),
+					'Sertifikasi' => ($n['hasil_akhir'][1]['hasil'] * $kriteria[1]['bobot']),
+					'Bahasa' => ($n['hasil_akhir'][2]['hasil'] * $kriteria[2]['bobot']),
+					'TPA' => ($n['hasil_akhir'][2]['hasil'] * $kriteria[3]['bobot']),
+					'Nilai_Skp' => ($n['hasil_akhir'][2]['hasil'] * $kriteria[4]['bobot']),
+					'total_nilai' => ($n['hasil_akhir'][0]['hasil'] * $kriteria[0]['bobot']) +
+					($n['hasil_akhir'][1]['hasil'] * $kriteria[1]['bobot']) +
+					($n['hasil_akhir'][2]['hasil'] * $kriteria[2]['bobot']) +
+					($n['hasil_akhir'][3]['hasil'] * $kriteria[3]['bobot']) +
+					($n['hasil_akhir'][4]['hasil'] * $kriteria[4]['bobot']),
+		
+				);
+				array_push($nilaiFinal,$val);
+				}
+
+				//   EXPORT TO CSV
+				  $delimiter = ","; 
+				  $filename = "SAW_" . date('Y-m-d') . ".csv"; 
+				  $f = fopen('php://memory', 'w'); 
+				  $fields = array();
+				  foreach ($nilaiFinal[0] as $k => $a){
+					array_push($fields,$k);
+					
+				  };
+				  fputcsv($f, $fields, $delimiter); 
+				  foreach($nilaiFinal as $n){
+					fputcsv($f, $n, $delimiter); 
+				  }
+
+				  fseek($f, 0); 
+				  header('Content-Type: text/csv'); 
+				  header('Content-Disposition: attachment; filename="' . $filename . '";'); 
+				   
+				  fpassthru($f); 
+			exit; 
+		}
+	
+	}
+   
+
+}
---
+++ application/views/index.php
@@ -0,0 +1,181 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+	<meta name="description" content="Responsive Admin &amp; Dashboard Template based on Bootstrap 5">
+	<meta name="author" content="AdminKit">
+	<meta name="keywords" content="adminkit, bootstrap, bootstrap 5, admin, dashboard, template, responsive, css, sass, html, theme, front-end, ui kit, web">
+
+	<link rel="preconnect" href="https://fonts.gstatic.com">
+	<link rel="shortcut icon" href="img/icons/icon-48x48.png" />
+
+	<link rel="canonical" href="https://demo-basic.adminkit.io/" />
+
+	<title>Tim KMeans BPK </title>
+
+	<link href="assets/static/css/app.css" rel="stylesheet">
+	<link href="https://fonts.googleapis.com/css2?family=Inter:wght@300;400;600&display=swap" rel="stylesheet">
+</head>
+
+<body>
+	<div class="wrapper">
+		<nav id="sidebar" class="sidebar js-sidebar">
+			<div class="sidebar-content js-simplebar">
+				<a class="sidebar-brand" href="index.html">
+          <span class="align-middle">TIM KMeans BPK</span>
+        </a>
+
+				<ul class="sidebar-nav">
+					<li class="sidebar-header">
+						Pages
+					</li>
+
+					<li class="sidebar-item active">
+						<a class="sidebar-link" href="index.html">
+              <i class="align-middle" data-feather="sliders"></i> <span class="align-middle">Dashboard</span>
+            </a>
+					</li>
+
+				
+				</ul>
+
+				<!-- <div class="sidebar-cta">
+					<div class="sidebar-cta-content">
+						<strong class="d-inline-block mb-2">Upgrade to Pro</strong>
+						<div class="mb-3 text-sm">
+							Are you looking for more components? Check out our premium version.
+						</div>
+						<div class="d-grid">
+							<a href="upgrade-to-pro.html" class="btn btn-primary">Upgrade to Pro</a>
+						</div>
+					</div>
+				</div> -->
+			</div>
+		</nav>
+
+		<div class="main">
+			<nav class="navbar navbar-expand navbar-light navbar-bg">
+				<a class="sidebar-toggle js-sidebar-toggle">
+          <i class="hamburger align-self-center"></i>
+        </a>
+
+				<div class="navbar-collapse collapse">
+					<ul class="navbar-nav navbar-align">
+						<!-- <li class="nav-item dropdown">
+							<a class="nav-icon dropdown-toggle" href="#" id="alertsDropdown" data-bs-toggle="dropdown">
+								<div class="position-relative">
+									<i class="align-middle" data-feather="bell"></i>
+									<span class="indicator">4</span>
+								</div>
+							</a>
+							
+						</li>
+						<li class="nav-item dropdown">
+							<a class="nav-icon dropdown-toggle" href="#" id="messagesDropdown" data-bs-toggle="dropdown">
+								<div class="position-relative">
+									<i class="align-middle" data-feather="message-square"></i>
+								</div>
+							</a>
+							
+						</li> -->
+						<!-- <li class="nav-item dropdown">
+							<a class="nav-icon dropdown-toggle d-inline-block d-sm-none" href="#" data-bs-toggle="dropdown">
+                <i class="align-middle" data-feather="settings"></i>
+              </a>
+
+							<a class="nav-link dropdown-toggle d-none d-sm-inline-block" href="#" data-bs-toggle="dropdown">
+                <span class="text-dark">Charles Hall</span>
+              </a>
+							<div class="dropdown-menu dropdown-menu-end">
+								<a class="dropdown-item" href="pages-profile.html"><i class="align-middle me-1" data-feather="user"></i> Profile</a>
+								<a class="dropdown-item" href="#"><i class="align-middle me-1" data-feather="pie-chart"></i> Analytics</a>
+								<div class="dropdown-divider"></div>
+								<a class="dropdown-item" href="index.html"><i class="align-middle me-1" data-feather="settings"></i> Settings & Privacy</a>
+								<a class="dropdown-item" href="#"><i class="align-middle me-1" data-feather="help-circle"></i> Help Center</a>
+								<div class="dropdown-divider"></div>
+								<a class="dropdown-item" href="#">Log out</a>
+							</div>
+						</li> -->
+					</ul>
+				</div>
+			</nav>
+
+			<main class="content">
+				<div class="container-fluid p-0">
+
+					<h1 class="h3 mb-3"><strong>Tools</strong> Perhitungan SAW </h1>
+
+									<div class="row">
+						<div class="col-xl-6 col-xxl-6 d-flex">
+						<div class="card flex-fill w-100">
+								<div class="card-header">
+
+									<h5 class="card-title mb-0">Hitung SAW</h5>
+								</div>
+								<div class="card-body py-3">
+								<?php echo form_open_multipart('CDashboard/do_upload');?>
+									<label class="form-label">Upload File</label>
+										<div class="row">
+										<div class="mb-3" style="width:70% ;">
+											
+											<input class="form-control form-control-lg" type="file" name="userfile" >
+										</div>
+								
+										<div class="text-center mb-3 w-30" style="width:30% ;">
+										<input type="submit" class="btn btn-primary" name="importSubmit" value="upload">
+										</div>
+
+										</div>
+										
+									</form>
+								</div>
+							</div>
+						</div>
+
+					</div>
+
+				
+
+				</div>
+			</main>
+
+			<footer class="footer">
+				<div class="container-fluid">
+					<div class="row text-muted">
+						<div class="col-6 text-start">
+							<p class="mb-0">
+								<a class="text-muted" href="https://adminkit.io/" target="_blank"><strong>AdminKit</strong></a> - <a class="text-muted" href="https://adminkit.io/" target="_blank"><strong>Bootstrap Admin Template</strong></a>								&copy;
+							</p>
+						</div>
+						<div class="col-6 text-end">
+							<ul class="list-inline">
+								<li class="list-inline-item">
+									<a class="text-muted" href="https://adminkit.io/" target="_blank">Support</a>
+								</li>
+								<li class="list-inline-item">
+									<a class="text-muted" href="https://adminkit.io/" target="_blank">Help Center</a>
+								</li>
+								<li class="list-inline-item">
+									<a class="text-muted" href="https://adminkit.io/" target="_blank">Privacy</a>
+								</li>
+								<li class="list-inline-item">
+									<a class="text-muted" href="https://adminkit.io/" target="_blank">Terms</a>
+								</li>
+							</ul>
+						</div>
+					</div>
+				</div>
+			</footer>
+		</div>
+	</div>
+
+	<script src="assets/static/js/app.js"></script>
+
+	
+
+</body>
+
+</html>
